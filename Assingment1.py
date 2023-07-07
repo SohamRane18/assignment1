1f05ce7db0117c8e7d6ef1b743bcee6643ad6db2
SohamRane18 <138582493+SohamRane18@users.noreply.github.com>
2023-07-07 23:31:36 +0530

    Add files via upload

---
+++ Assingment1.py
@@ -0,0 +1,7 @@
+a = int (input(" Please Enter the 1st Number: ")) 
+b = int (input(" Please Enter the 2nd number: "))
+c = int (input(" Please Enter the 3rd number: "))
+
+average=(a+b+c)/3
+
+print("The average is ", average)
